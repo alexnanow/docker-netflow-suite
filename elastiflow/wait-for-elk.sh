5f0df79fb7b7fe812ee254be6cc5c3153b26891d
alexnanow <53272937+alexnanow@users.noreply.github.com>
2021-11-03 19:20:05 -0300

    Tradução das mensagens de espera do ELK

--- elastiflow/wait-for-elk.sh
+++ elastiflow/wait-for-elk.sh
@@ -2,7 +2,7 @@
 set -e
 echo "Initiating..."
 until curl --output /dev/null --silent --head --fail "$ELK_URL"; do
-  >&2 echo "ELK ainda esta - aguardando 5s"
+  >&2 echo "ELK ainda esta down - aguardando 5s"
   sleep 5
 done
 >&2 echo "ELK esta up!"
