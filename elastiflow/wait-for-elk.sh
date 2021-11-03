0eede80416a7094b871e1be31028974b821e35f1
alexnanow <53272937+alexnanow@users.noreply.github.com>
2021-11-03 17:44:23 -0300

    Create wait-for-elk.sh

---
+++ elastiflow/wait-for-elk.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+echo "Initiating..."
+until curl --output /dev/null --silent --head --fail "$ELK_URL"; do
+  >&2 echo "ELK is unavailable - sleeping"
+  sleep 1
+done
+>&2 echo "ELK is up"
+source /usr/local/bin/flowcoll
